32ba620367df049d02b1589b7bdc8d5374938b0d
AthiraAmbujakshan <63126142+AthiraAmbujakshan@users.noreply.github.com>
2022-10-17 12:00:50 +0530

    Add files via upload

---
+++ Pratise/src/index.js
@@ -0,0 +1,50 @@
+/* import React from 'react';
+
+ import ReactDOM from 'react-dom';
+
+
+
+ //import ParentComponent  from './Components/ParentComponent';
+
+//import Stylesheet from './CSSStyling/Stylesheet';
+//import InlineStyle from './CSSStyling/InlineStyle';
+//import './appstyle.css';
+//import styles from './appstyle.module.css'
+
+
+
+ class App extends React.Component {
+
+	ender(){
+		return(
+
+			//<div><ParentComponent  /></div>
+			//<div> <Stylesheet /></div>
+			//-----using props/states
+			//<div> <Stylesheet primary={true} /></div>
+			//<div> <InlineStyle /></div>
+			  <div>
+		
+                    <InlineStyle />
+			 <h1 className  ='error'>ERROR </h1> 
+			<h1 className ={styles.success}>SUCCESS</h1>
+
+			 </div> 
+			 <div>
+
+			 </div>
+			
+
+		)
+	}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));  
+ 
+ 
+ */
+
+
+ 
+ 
